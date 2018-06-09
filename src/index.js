94141a909123204fdb0b33fef52fd6be1c4d1500
david <dkirkman@gmail.com>
2018-06-09 13:16:16 -0700

    make dynamic imports work with both webpack 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import libTest from './libTest.js';
+//import libTest from './libTest.js';
 import libTest_wasm from './libTest.wasm';
+//import libTestAsm from './libTestAsm.js';
 
 // Same set of tests emscripten uses to work out where it is
 const ENVIRONMENT_IS_WEB = typeof window === 'object';
@@ -7,12 +8,6 @@ const ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 const ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function' && !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_WORKER;
 
 export default function(options, cb) {
-  let url_prefix = '';
-  if (typeof options === 'object' 
-      && options['url_prefix'] !== undefined) {
-    url_prefix = options['url_prefix'];
-    if (url_prefix.slice(-1) !== '/') url_prefix += '/';
-  }
 
   let module_options = {
     locateFile: function (path) {
@@ -20,24 +15,32 @@ export default function(options, cb) {
         if (ENVIRONMENT_IS_NODE) {
           return (__dirname + '/' + libTest_wasm);
         } else {
-          return (url_prefix + libTest_wasm);
+          return (libTest_wasm);
         }
       }
       return path;
     }
-
   };
 
   if (typeof options === 'object'
       && options['asm.js'] === true) {
-    // loading asm.js
+    console.log('loading asm.js version of libTest');
+
     import(/* webpackChunkName: "libTestAsm" */ 
       './libTestAsm.js').then(module => {
-        module.default(module_options).then(cspace => cb(cspace));
+        let cons = module.default ? module.default : module;
+        cons(module_options).then(cspace => cb(cspace));
       });
+
   } else {
     // loading webpack
-    libTest(module_options).then(cspace => cb(cspace));
+    console.log('loading webpack version of libTest boot boot toot');
+
+    import(/* webpackChunkName: "libTest" */ 
+      './libTest.js').then(module => {
+        let cons = module.default ? module.default : module;
+        cons(module_options).then(cspace => cb(cspace));
+      });
   }
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: '[name]-[hash].[ext]',         
-        }
+        }        
       }
     ]
   },
