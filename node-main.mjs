3dc6874ad9635c99c549e544dd3eb975efb33ead
david <dkirkman@gmail.com>
2018-06-12 09:52:13 -0700

    node test program

--- node-main.mjs
+++ node-main.mjs
@@ -1,6 +1,6 @@
 import ttest from './build/index.mjs';
 
 ttest({'asm.js': true}, cspace => {
-  console.log(cspace._add_values(33, 55));
+  console.log('33 + 55 is ' + cspace._add_values(33, 55));
 });
 
