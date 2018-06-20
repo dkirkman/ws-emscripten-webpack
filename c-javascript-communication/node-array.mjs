5ea86f9918190c85a4b95432aae7855fec986da7
david <dkirkman@gmail.com>
2018-06-20 11:55:36 -0700

    add some timing of the compile and instantiate phases

--- c-javascript-communication/node-array.mjs
+++ c-javascript-communication/node-array.mjs
@@ -6,9 +6,10 @@ import path from 'path';
 let filename = path.normalize("./build/libTest.wasm");
 let wasmBytes = fs.readFileSync(filename);
 
+import {performance} from 'perf_hooks';
 
 function allocCSpaceArray(cSpace, arrayConstructor, ndat) {
-  let size = ndat * 8;
+  let size = ndat * arrayConstructor.BYTES_PER_ELEMENT;
   let cPtr = cSpace._malloc(size);
   let jsArray = new arrayConstructor(cSpace.HEAPU8.buffer, cPtr, ndat);
 
@@ -28,9 +29,15 @@ function initializeArray(array) {
   }
 }
 
+let pre_compile = performance.now();
 WebAssembly.compile(wasmBytes).then(wasmModule => {
+  let post_compile = performance.now();
   function instantiateWasm(info, callback) {
+    let pre_instantiate = performance.now();
     WebAssembly.instantiate(wasmModule, info).then(instance => {
+      let post_instantiate = performance.now();
+      console.log('compile: ' + (post_compile - pre_compile));
+      console.log('instantiate: ' + (post_instantiate - pre_instantiate));
       callback(instance);
     });
 
@@ -55,8 +62,8 @@ WebAssembly.compile(wasmBytes).then(wasmModule => {
 
 
   single_run();
-  setTimeout(single_run, 3000);  // load it a second time, but don't reparse
-                                 // bytecode.
+//  setTimeout(single_run, 3000);  // load it a second time, but don't reparse
+//                                 // bytecode.
 });
 
 
